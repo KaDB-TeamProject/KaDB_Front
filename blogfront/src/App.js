35e60a0dd0bcdde316f014a5280f4a0346a8456e
강석훈 <hamer1233@naver.com>
2023-10-28 13:55:55 +0900

    KSH 20231028 블로그리스트,개인페이지,로그인,NavBar,장소,비번설정,회원가입'

--- blogfront/src/App.js
+++ blogfront/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/Integrated';
+import BlogList from './components/BlogList/Integrated';
+import Login from './components/Login/Integrated';
+import PWResetL from './components/PWResetL/Integrated';
+import SignUp from './components/SignUp/Integrated';
+import Individual from './components/Individual/Integrated';
+import PlaceInfo from './components/PlaceInfo/Integrated';
 
 function App() {
   return (
-      <NavBar/>
+      <PlaceInfo/>
   );
 }
 
--- blogfront/src/components/NavBar/SignButton.js
+++ blogfront/src/components/NavBar/SignButton.js
@@ -5,11 +5,11 @@ const SB = styled.button`
     color : black;
     border : none;
     border-radius : 10px;
-    width : 3rem;
+    width : 3.5rem;
     font-size : 0.4rem;
     padding : 0.3rem;
     margin-top : 1rem;
-    margin-botton : 1rem;
+    font-weight : bold;
 `
 
 function SignButton(props){
