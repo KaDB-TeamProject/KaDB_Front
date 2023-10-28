9f95441d68d7cc7604e5eebc0a994da9d302abb3
hogishim <harrythegreat@gachon.ac.kr>
2023-10-28 16:19:15 +0900

    HC Routing기능 추가 이후 페이지 수정

--- blogfront/src/components/BadgeRq/BadgeBar.js
+++ blogfront/src/components/BadgeRq/BadgeBar.js
@@ -3,33 +3,46 @@ import img1 from './Source/ads.png'
 import img2 from './Source/inf.png'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
+import settingImg from '../Setting/Source/settings.png'
 
 let Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
+  width: 100vw;
 
 
 `
 let SettingBox = styled.div`
 
-    width: 40vw;
-  height: 50vh;
+    width: 30vw;
+  height: 45vh;
   text-align: center;
   background-color: #FFDB31;
   border: 1px solid rgb(233, 243, 40);
   border-radius: 20px;
 
 `
+let HeaderContainer = styled.div`
+
+flex-grow: 10;
+justify-content: center;
+width: 100%;
+height: 10vh;
+display: flex;
+flex-direction: row;
+
+
+`
+
 let RequestBadge = styled.div`
 
 display: flex;
   width: 100%;
-  height: 70%;
+  height: 30vh;
   justify-content: center;
   align-items: center;
-
 
 
 `
@@ -38,8 +51,8 @@ let AddBadge = styled.div`
 display: flex;
   justify-content: center;
   align-items: center;
-  width: 15vw;
-  height: 15vw;
+  width: 12vw;
+  height: 12vw;
   background-color: snow;
   border-radius: 50%;
   margin: 3px;
@@ -51,8 +64,8 @@ let InBadge = styled.div`
 display: flex;
   justify-content: center;
   align-items: center;
-  width: 15vw;
-  height: 15vw;
+  width: 12vw;
+  height: 12vw;
   background-color: snow;
   border-radius: 50%;
   margin: 3px;
@@ -90,6 +103,35 @@ width: 30%;
 
 `
 
+let ImgBox2 = styled.div`
+
+flex-grow: 3;
+display: flex;
+align-items: center;
+justify-content: center;
+
+`
+
+let BackImg = styled.img`
+
+width: 50px;
+height: 50px;
+
+
+
+`
+let H = styled.div`
+
+flex-grow: 7;
+display: flex;
+align-items: center;
+justify-content: center;
+font-size: 20px;
+
+
+`
+
+
 function BadgeBar() {
 
     let navi = useNavigate();
@@ -101,7 +143,12 @@ function BadgeBar() {
                 {/* setting할 수 있는 메뉴를 불러오는데 props를 이용하여 설정 */}
                 <SettingBox>
 
-                    <h3>Badge 요청하기</h3>
+                <HeaderContainer>
+                        <H>비밀번호 재설정</H>
+                        <ImgBox2>
+                        <BackImg src={settingImg} onClick={()=>{navi("/setting")}}/>
+                        </ImgBox2>
+                    </HeaderContainer>
                     <RequestBadge>
                         <AddBadge>
                             <ImgBox>
--- blogfront/src/components/PWResetS/PWBox.js
+++ blogfront/src/components/PWResetS/PWBox.js
@@ -12,6 +12,7 @@ let Container = styled.div`
     justify-content: center;
     align-items: center;
     height: 100vh;
+    width: 100vw;
 
 `
 
