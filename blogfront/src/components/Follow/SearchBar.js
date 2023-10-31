a2dfa82afc302bcd832bc4f7f0257773ac0db7ea
hogishim <harrythegreat@gachon.ac.kr>
2023-10-31 20:27:15 +0900

    HC follow 주석 추가

--- blogfront/src/components/Follow/SearchBar.js
+++ blogfront/src/components/Follow/SearchBar.js
@@ -9,12 +9,21 @@ const Input = styled.input`
     outline: none;
 `;
 
+//as props, it will recieve props for storing search data that will be handed in
+//to upper component that called this component
+//value will be storing data, and onChange will hand in search data to the upper component
 function SearchBar({ value, onChange }) {
+
+    // function for storing input. When input is changed, it will store it into state data
     const handleSearch = (event) => {
+        //set search term as current value
         const searchTerm = event.target.value;
+        //hand it to upper component
         onChange(searchTerm);
     };
 
+    //inputbox. user will put data into the input and on changed, it will call handlesearch
+    //value will be stored
     return <Input type="text" placeholder="사용자를 검색하세요"
     value={value} onChange={handleSearch} />;
 }
