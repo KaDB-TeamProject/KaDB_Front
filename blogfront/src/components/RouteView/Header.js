e91fbc99127e2287c1cb5f910aa5b4c18aff2ad4
hogishim <harrythegreat@gachon.ac.kr>
2023-11-29 20:25:16 +0900

    HC 29일 작업 중간 저장

---
+++ blogfront/src/components/RouteView/Header.js
@@ -0,0 +1,106 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components'
+
+const Container = styled.div`
+
+height: 40vh;
+width: 70vw;
+
+`
+
+const SiteName = styled.div`
+height: 15vh;
+ font-family: KakaoBold;
+ font-size: 10vh;
+ color: #FAF4C0;
+ font-weight: bold;
+`;
+
+const BlogDescription = styled.div`
+height: 5vh;
+  font-size: 2vh;
+  font-family: KakaoRegular;
+  color: #FFFFFF;
+`;
+
+const ScheduleTitle = styled.div`
+  height: 5vh;
+  width: 70vw;
+  font-size: 12pt;
+  font-family: KakaoBold;
+  color: #FFFFFF;
+  font-weight: bold;
+  text-decoration: 2px yellow underline;
+
+`;
+
+
+
+const Schedule = styled.div`
+height: 10vh;
+width: 70vw;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+
+
+const ScheduleBox = styled.div`
+  background-color: #FFFFFF;
+  height: 7vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40vw;
+  color: #000000;
+  border-radius: 50px;
+  font-family: KakaoBold;
+  font-size: 10pt;
+  font-weight: bold;
+`;
+
+const Copybutton = styled.button`
+  width: 7rem;
+  height: 7vh;
+  background-color: #FFFFFF;
+  color: #000000;
+  font-family: KakaoBold;
+  font-size: 10pt;
+  border-radius: 50px;
+  font-weight: bold;
+  text-shadow: 2px 2px 2px gray;
+
+`;
+
+function Header() {
+
+    const [date, setDate] = useState('2019.07.07 ~ 2019.08.13');
+    const[title, setTitle] = useState('나의 첫 유럽 6개국 여행')
+
+    const navi = useNavigate();
+
+    return (
+
+        <Container>
+            <div>
+                <SiteName>Trip Again</SiteName>
+                <BlogDescription>좌충우돌 세계일주 여행기</BlogDescription>
+            </div>
+            <div>
+                <ScheduleTitle>{date}</ScheduleTitle>
+                <Schedule>
+                    <ScheduleBox>{title}</ScheduleBox>
+                    <Copybutton onClick={() => { navi(`/manageroute`) }}>목록으로</Copybutton>
+                    <Copybutton onClick={() => { navi(`/routeedit`) }}>일정 수정</Copybutton>
+                </Schedule>
+            </div>
+        </Container>
+
+    )
+
+
+
+}
+export default Header;
